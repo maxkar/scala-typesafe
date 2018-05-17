afcbc3156fce0d9c19c91b405dddeeda53b6785f
Alexey Skorokhodov <askorokhodov@twilio.com>
2018-05-16 18:35:26 -0700

    deleted extra code

--- src/main/scala/com/example/storage/CirceBoilerplateForConfigs.scala
+++ src/main/scala/com/example/storage/CirceBoilerplateForConfigs.scala
@@ -1,15 +1,10 @@
 package com.example.storage
 
-import com.example.model.{DateField, Field, FieldMapping, StringField}
+import com.example.model.{Field, FieldMapping}
 import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
 import io.circe.{Decoder, Encoder}
 
 object CirceBoilerplateForConfigs {
-  implicit val stringDecoder: Decoder[StringField] = deriveDecoder[StringField]
-  implicit val stringEncoder: Encoder[StringField] = deriveEncoder[StringField]
-
-  implicit val dateDecoder: Decoder[DateField] = deriveDecoder[DateField]
-  implicit val dateEncoder: Encoder[DateField] = deriveEncoder[DateField]
 
   implicit val fieldDecoder: Decoder[Field[_]] = deriveDecoder[Field[_]]
   implicit val fieldEncoder: Encoder[Field[_]] = deriveEncoder[Field[_]]
